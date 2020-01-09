d29588d41c9982c332bc8a748170b3e92e544964
Jackie <JGtheophila19@gmail.com>
2020-01-09 11:38:01 -0500

    getting the current date and time

--- CsharpFundamentals/Program.cs
+++ CsharpFundamentals/Program.cs
@@ -10,10 +10,11 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-			var dateTime = new DateTime(2015, 1, 1);
-            
-            /* datetime represents dates and times with values ranging from 00:00:00 (midnight), January 1, 0001 Anno 
-            Domini (Common Era) through 11:59:59 P.M., December 31, 9999 A.D. (C.E.) in the Gregorian calendar*/
+		var dateTime = new DateTime(2015, 1, 1);
+		var now = DateTime.Now;
+		
+		/* Gets a DateTime object that is set to the current date 
+		and time on a computer, expressed as the local time.*/
         }
     }
 }
