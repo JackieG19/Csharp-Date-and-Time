2de326b8bdf8743056df350aea4844b730d5fe6b
Jackie <JGtheophila19@gmail.com>
2020-01-09 13:10:47 -0500

    Returns a TimeSpan that represents a...
     specified number of hours, where the specification is accurate to the nearest millisecond.

--- CsharpFundamentals/Program.cs
+++ CsharpFundamentals/Program.cs
@@ -12,11 +12,11 @@ namespace HelloWorld
         {
 		var timeSpan = new TimeSpan(1,2,3);
 		var timeSpan1 = new TimeSpan(1,0,0);
-		
-		Console.WriteLine(timeSpan);
-		Console.WriteLine(timeSpan1);
-		/*Initializes a new instance of the TimeSpan structure 
-		to a specified number of hours, minutes, and seconds.*/
+
+		var timeSpan2 = TimeSpan.FromHours(1);
+		Console.WriteLine(timeSpan2);
+		/*This method is used to get a TimeSpan that represents a specified 
+		number of hours, accurate to the nearest millisecond.*/
         }
     }
 }
