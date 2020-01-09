a41613d77316c8e8feec4c832d9069e4a3306536
Jackie <JGtheophila19@gmail.com>
2020-01-09 11:34:19 -0500

    Initializing a DateTime

--- CsharpFundamentals/Program.cs
+++ CsharpFundamentals/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-/*using statement*/
 
 namespace HelloWorld
 {
@@ -11,7 +10,10 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+			var dateTime = new DateTime(2015, 1, 1);
+            
+            /* datetime represents dates and times with values ranging from 00:00:00 (midnight), January 1, 0001 Anno 
+            Domini (Common Era) through 11:59:59 P.M., December 31, 9999 A.D. (C.E.) in the Gregorian calendar*/
         }
     }
 }
