1d95b80fead9eb15f437c4ec90d672636fdf862f
Jackie <JGtheophila19@gmail.com>
2020-01-09 12:02:53 -0500

    Converts the value of the current DateTime to...
    equivalent long date string representation.

--- CsharpFundamentals/Program.cs
+++ CsharpFundamentals/Program.cs
@@ -12,15 +12,11 @@ namespace HelloWorld
         {
 		var dateTime = new DateTime(2015, 1, 1);
 		var now = DateTime.Now;
+		var today = DateTime.Today;
 		
-		var tomorrow = now.AddDays(1);
-		Console.Write(tomorrow);
-		
-		var yesterday = now.AddDays(-1);
-		Console.Write(yesterday);
-		
-		/*This method is used to return a new DateTime that adds or sbubtracts 
-		the specified number of days to the value of this instance.*/
+		Console.WriteLine(now.ToLongDateString);
+		/*This method is used to convert the value of the current DateTime 
+		object to its equivalent long date string representation.*/
         }
     }
 }
