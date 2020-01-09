5aefd415b5a9eb663d386592ad8452757a7f665c
Jackie <JGtheophila19@gmail.com>
2020-01-09 13:05:51 -0500

    Initializes a new instance of the TimeSpan

--- CsharpFundamentals/Program.cs
+++ CsharpFundamentals/Program.cs
@@ -10,13 +10,13 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-		var dateTime = new DateTime(2015, 1, 1);
-		var now = DateTime.Now;
-		var today = DateTime.Today;
+		var timeSpan = new TimeSpan(1,2,3);
+		var timeSpan1 = new TimeSpan(1,0,0);
 		
-		Console.WriteLine(now.ToString("yyyy-MM-dd HH:mm"));
-		/*This method is used to Converts the value of the current 
-		DateTime object to its equivalent string representation.*/
+		Console.WriteLine(timeSpan);
+		Console.WriteLine(timeSpan1);
+		/*Initializes a new instance of the TimeSpan structure 
+		to a specified number of hours, minutes, and seconds.*/
         }
     }
 }
