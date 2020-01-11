7b5eff8351b2d805d1fdcaef1b6b696ea363cff4
Jackie <JGtheophila19@gmail.com>
2020-01-11 13:45:26 -0500

    Returning the minute component of the current...
    TimeSpan structure value ranges from -59 through 59.

--- CsharpFundamentals/Program.cs
+++ CsharpFundamentals/Program.cs
@@ -11,15 +11,11 @@ namespace HelloWorld
         static void Main(string[] args)
         {
 		var timeSpan = new TimeSpan(1,2,3);
-		var timeSpan1 = new TimeSpan(1,0,0);
-
-		var start = DateTime.Now;
-		var end = DateTime.Now.AddMinutes(2);
-		/*This method is used to return a new DateTime that adds 
-		the specified number of minutes to the value of this instance.*/
 		
-		var duration = end - start;
-		Console.WriteLine("Duration" + duration);
+		// Properties
+		Console.WriteLine("Minutes" + timeSpan.Minutes);
+		/*Getting the minutes component of the time interval 
+		represented by the current TimeSpan structure.*/
         }
     }
 }
