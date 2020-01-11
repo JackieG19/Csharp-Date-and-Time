5efba1292075157eeaf5feb40e15f931473befd6
Jackie <JGtheophila19@gmail.com>
2020-01-11 13:58:02 -0500

    Converting the value of the current TimeSpan...
    object to its equivalent string representation.

--- CsharpFundamentals/Program.cs
+++ CsharpFundamentals/Program.cs
@@ -12,13 +12,9 @@ namespace HelloWorld
         {
 		var timeSpan = new TimeSpan(1,2,3);
 		
-		// Add and Subtract
-		Console.WriteLine("Add Example" + timeSpan.Add(TimeSpan.FromMinutes(8)));
-		/*The Add() method in C# is used to return a new TimeSpan object whose 
-		value is the sum of the specified TimeSpan object and this instance.*/
-		
-		Console.WriteLine("Subtract Example" + timeSpan.Subtract(TimeSpan.FromMinutes(2)));
-		/*This method is used to subtract the specified time or duration from this instance.*/
+		// ToString
+		Console.WriteLine("ToString" + timeSpan.ToString());
+		//Converts the value of the current TimeSpan object to its equivalent string representation.
         }
     }
 }
